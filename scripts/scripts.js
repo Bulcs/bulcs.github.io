f708c100d185221c632bf5465c7fe8ec05600ad7
Bulcs <vrbulcao@hotmail.com>
2021-03-23 19:04:56 -0300

    Responsive menu ended

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {scrollFunction()};
 
 		function scrollFunction() {
-		  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+		  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200 || (document.getElementById('header-button').getAttribute('aria-expanded') == true && window.getComputedStyle(document.getElementById("header-button")).display != "none")) {
 		  	document.getElementById("header").style.paddingBottom = "20px";
 		    document.getElementById("header").style.backgroundColor = "rgba(242, 242, 242, 1)" ;
 		    document.getElementById("header").style.transition = "all 2s";
@@ -9,12 +9,6 @@ window.onscroll = function() {scrollFunction()};
 		  } else {
 		  	document.getElementById("header").style.boxShadow = "0 8px 6px -6px rgba(0,0,0,0)";
 		  	document.getElementById("header").style.paddingBottom = "0px";
-		    document.getElementById("header").style.backgroundColor = "rgba(0,0,0,0)";
-		  }
-
-		  if (document.getElementById('header-button').getAttribute('aria-expanded') == true) {
-		  	document.getElementById("header").style.backgroundColor = "rgba(242, 242, 242, 1)";
-		  }else{
-		  	document.getElementById("header").style.backgroundColor = "rgba(0,0,0,0)";
+		    document.getElementById("header").style.backgroundColor = "transparent";
 		  }
 		}
